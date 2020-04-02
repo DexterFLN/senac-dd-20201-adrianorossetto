8533eb2edead599d0d75213796a5d4ec7ed1e9f1
rosse <rosse@LAPTOP-FT74NHVR>
2020-04-02 17:42:12 -0300

    Falta corrigir as listagens e algumas integrações de tela.

--- src/main/java/controller/exercicio01/ClienteController.java
+++ src/main/java/controller/exercicio01/ClienteController.java
@@ -2,50 +2,98 @@ package controller.exercicio01;
 
 import java.util.ArrayList;
 
+import javax.swing.JOptionPane;
+
 import model.bo.exercicio01.ClienteBO;
+
 import model.dao.exercicio01.ClienteDAO;
 import model.entity.exercicio01.Cliente;
 import model.entity.exercicio01.Endereco;
 
+
 public class ClienteController {
 
-	private ClienteDAO dao = new ClienteDAO();
 	private ClienteBO bo = new ClienteBO();
+	
+	private ClienteDAO dao = new ClienteDAO();
 
 	public ArrayList<Cliente> listarTodosOsClientes() {
 		return dao.consultarTodos();
 	}
 	
-	public String excluir(String textoIdSelecionado) {
+	public String excluir(Cliente cliente) {
 		String mensagem = "";
 		try {
-			int idSelecionado = Integer.parseInt(textoIdSelecionado);
-			mensagem = bo.excluir(idSelecionado);
+			mensagem = bo.excluir(cliente);
 		} catch (NumberFormatException ex) {
 			mensagem = "Informe um número inteiro";
 		}
 		return mensagem;
 	}
-
-	public String salvar(String txtNome, String txtSobrenome, String txtCpf, Object txtEndereco) {
+	
+	public String validarSobrenome(String sobrenome) {
 		String mensagem = "";
 		
-		ClienteBO bo = new ClienteBO();
-		Cliente cliente = criarCliente(txtNome, txtSobrenome, txtCpf, txtEndereco);
-		bo.salvar(cliente);
+		if(sobrenome.isEmpty()) {
+			mensagem = "O campo SOBRENOME não pode ficar vazio.\n";
+			JOptionPane.showMessageDialog(null, mensagem);
+		}
+		
+		
 		return mensagem;
-		
-	}
-
-	private Cliente criarCliente(String txtNome, String txtSobrenome, String txtCpf, Object txtEndereco) {
-		Cliente cliente = new Cliente();
-		cliente.setNome(txtNome);
-		cliente.setSobrenome(txtSobrenome);
-		cliente.setCpf(txtCpf);
-		
-		Endereco endereco = (Endereco) txtEndereco;
-		cliente.setEndereco(endereco);
-		return cliente;
 	}
 	
+	public String validarCpf(String cpf) {
+		String mensagem = "";
+		
+		if(cpf.length() != 11) {
+			mensagem = "O campo CPF não pode ter mais de 11 dígitos.\n";
+			JOptionPane.showMessageDialog(null, mensagem);
+		}
+		
+		return mensagem;
+	}
+	
+	public String validarNome(String nome) {
+		String mensagem = "";
+		
+		if(nome.isEmpty()) {
+			mensagem = "O campo NOME não pode ficar vazio.\n";
+			JOptionPane.showMessageDialog(null, mensagem);
+		}
+		return mensagem; 
+	}
+	
+	public String validarCpfObrigatorio(String cpf) {
+		String mensagem = "";
+		
+		if(cpf.isEmpty()) {
+			mensagem = "O campo CPF não pode ficar vazio.\n";
+			JOptionPane.showMessageDialog(null, mensagem);
+		}
+		return mensagem; 
+	}
+	
+	public String salvarCliente(String txtNome, String txtSobrenome, String txtCpf, Object txtEndereco) {
+        String mensagem = "";
+        
+        
+
+        ClienteBO bo = new ClienteBO();
+        Cliente cliente = criarCliente(txtNome, txtSobrenome, txtCpf, txtEndereco);
+        bo.salvar(cliente);
+        return mensagem;
+    }
+	
+    public Cliente criarCliente(String txtNome, String txtSobrenome, String txtCpf, Object txtEndereco) {
+        Cliente cliente = new Cliente ();
+        cliente.setNome(txtNome);
+        cliente.setSobrenome(txtSobrenome);
+        cliente.setCpf(txtCpf);
+
+        Endereco endereco = (Endereco)txtEndereco;
+        cliente.setEndereco(endereco);
+
+        return cliente;
+    }
 }
--- src/main/java/model/bo/exercicio01/ClienteBO.java
+++ src/main/java/model/bo/exercicio01/ClienteBO.java
@@ -1,7 +1,5 @@
 package model.bo.exercicio01;
 
-import java.util.ArrayList;
-
 import javax.swing.JOptionPane;
 
 import model.dao.exercicio01.ClienteDAO;
@@ -9,51 +7,48 @@ import model.entity.exercicio01.Cliente;
 
 public class ClienteBO {
 
-	private ClienteDAO dao;
-
-	public String excluir(int idSelecionado) {
+	private ClienteDAO dao = new ClienteDAO();
+	/**
+	 * Tenta salvar um cliente novo, validando o CPF
+	 * @param cliente o Cliente a ser salvo no banco
+	 * @return
+	 */
+	public String salvar(Cliente cliente) {
 		String mensagem = "";
+		
+		if(dao.cpfJaUtilizado(cliente.getCpf())) {
+			mensagem = "CPF informado (" + cliente.getCpf() + ") já foi utilizado";
+			JOptionPane.showMessageDialog(null, mensagem);
 
-		ClienteDAO clienteDAO = new ClienteDAO();
-		if (clienteDAO.temClienteCadastradoComEsteID(idSelecionado) == false) {
-			mensagem = "Cliente informado não existe no Banco de Dados. Informe um ID correto.";
-		} else {
-			if (clienteDAO.excluir(idSelecionado)) {
-				mensagem = "Cliente excluído com sucesso";
+		}else {
+			cliente = dao.salvar(cliente);
+			
+			if(cliente.getId() > 0) {
+				mensagem = "Cliente salvo com sucesso";
+				JOptionPane.showMessageDialog(null, mensagem);
+			}else {
+				mensagem = "Erro ao salvar cliente";
+				JOptionPane.showMessageDialog(null, mensagem);
+			}
+		}
+		
+		return mensagem;
+	}
+	
+	
+	public String excluir(Cliente cliente) {
+		String mensagem = "";
+		if (cliente.getTelefones().isEmpty()) {
+			if (dao.excluir(cliente)) {
+				mensagem = "Excluído com sucesso";
 			} else {
 				mensagem = "Erro ao excluir";
 			}
+		} else {
+			mensagem = "Cliente com cpf (" + cliente.getCpf() + ") possui telefone cadastrado.";
 		}
 
 		return mensagem;
 	}
-	
-	public ArrayList<Cliente> pesquisarTodos() {
-		return dao.consultarTodos();
-	}
 
-	public String salvar(Cliente cliente) {
-        String mensagem = "";
-
-        if(dao.cpfJaUtilizado(cliente.getCpf())) {
-            mensagem = "CPF informado (" + cliente.getCpf() + ") já foi utilizado";
-            JOptionPane.showMessageDialog(null, mensagem);
-
-        }else {
-            cliente = dao.salvar(cliente);
-
-            if(cliente.getId() > 0) {
-                mensagem = "Cliente salvo com sucesso";
-                JOptionPane.showMessageDialog(null, mensagem);
-            }else {
-                mensagem = "Erro ao salvar cliente";
-                JOptionPane.showMessageDialog(null, mensagem);
-            }
-        }
-
-        return mensagem;
-    }
-	
-
-	// TODO criar os métodos para chamar os métodos PÚBLICOS no ClienteDAO
 }
